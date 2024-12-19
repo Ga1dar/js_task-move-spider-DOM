39293683c8330a743a8a01963d89b7f40ea7358b
Kyrylo Golikov <pipecc1985@gmail.com>
2024-12-19 18:36:49 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
   const spider = document.querySelector('.spider');
   const spiderState = {
     x: 0,
     y: 0,
     element: spider,
   };
+
   const wall = document.querySelector('.wall');
   const wallRect = wall.getBoundingClientRect();
   const spiderWidth = spider.offsetWidth;
@@ -19,13 +19,26 @@ document.addEventListener('click', (e) => {
     e.clientY >= wallRect.top &&
     e.clientY <= wallRect.bottom
   ) {
-    spiderState.x = e.clientX - wallRect.left - spiderWidth / 2;
-    spiderState.y = e.clientY - wallRect.top - spiderHeight / 2;
+    spiderState.x = e.clientX - wallRect.left - (spiderWidth / 2 + 8);
+    spiderState.y = e.clientY - wallRect.top - (spiderHeight / 2 + 12);
+
+    if (spiderState.x < 0) {
+      spiderState.x = 0;
+    }
+
+    if (spiderState.y < 0) {
+      spiderState.y = 0;
+    }
+
+    if (spiderState.x > wallRect.width - spiderWidth) {
+      spiderState.x = wallRect.width - spiderWidth;
+    }
+
+    if (spiderState.y > wallRect.height - spiderHeight) {
+      spiderState.y = wallRect.height - spiderHeight;
+    }
 
     spider.style.left = `${spiderState.x}px`;
     spider.style.top = `${spiderState.y}px`;
-  }
-
-  if (e.target !== wall) {
   }
 });
